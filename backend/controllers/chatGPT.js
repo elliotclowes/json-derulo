2d8e589b6c2765f951950f813c08e741162ac559
elliotclowes <elliot@clowes.me>
2023-08-09 17:29:51 +0100

    axios-retry to improve ChatGPT reliability

--- backend/controllers/chatGPT.js
+++ backend/controllers/chatGPT.js
@@ -1,55 +1,74 @@
+// backend/controllers/chatGPT.js
 require('dotenv').config();
 const https = require('https');
 
 const apiKey = process.env.OPENAI_API_KEY;
 
-async function summarizeTranscript(transcriptText) {
+const axios = require('axios');
+const axiosRetry = require('axios-retry');
 
+axiosRetry(axios, {
+  retries: 3, // Number of retries
+  retryDelay: (retryCount) => {
+    return retryCount * 1000; // Time between retries increases by 1s
+  },
+  // Retry on StatusCode 500
+  retryCondition: (error) => {
+    return error.response && error.response.status === 500;
+  },
+});
+
+const axiosInstance = axios.create({
+  timeout: 60000, // Set timeout to 60 seconds (or adjust as needed)
+});
+axiosRetry(axiosInstance, {
+  retries: 3,
+  retryDelay: (retryCount) => {
+    return retryCount * 1000;
+  },
+  retryCondition: (error) => {
+    return error.response && error.response.status === 500;
+  },
+});
+
+async function summarizeTranscript(transcriptText) {
+  console.log("🚀 ~ file: chatGPT.js:40 ~ summarizeTranscript ~ apiKey:", apiKey);
+  console.log("🚀 ~ file: chatGPT.js:21 ~ summarizeTranscript ~ transcriptText:", transcriptText);
   const apiUrl = 'https://api.openai.com';
   const path = '/v1/engines/text-davinci-003/completions';
 
   // Create a prompt to ask ChatGPT to summarize the transcript text
   const prompt = `How many words are there?:\n\n${transcriptText}`;
 
-  const requestBody = JSON.stringify({
+  console.log("🚀 ~ file: chatGPT.js:28 ~ summarizeTranscript ~ prompt:", prompt);
+
+  const requestBody = {
     prompt: prompt,
     max_tokens: 4000
-  });
+  };
+  console.log("🚀 ~ file: chatGPT.js:37 ~ summarizeTranscript ~ requestBody:", requestBody);
 
   const options = {
-    hostname: 'api.openai.com',
-    path: path,
     method: 'POST',
+    url: apiUrl + path,
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${apiKey}`,
     },
+    data: requestBody
   };
 
-  return new Promise((resolve, reject) => {
-    const httpsRequest = https.request(options, (response) => {
-      let data = '';
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-      response.on('end', () => {
-        const responseJson = JSON.parse(data);
-        if (responseJson.error) {
-          reject(new Error(responseJson.error.message));
-        } else {
-          const summary = responseJson.choices[0].text.trim();
-          resolve(summary);
-        }
-      });
-    });
+  try {
+    const response = await axiosInstance(options);
 
-    httpsRequest.on('error', (error) => {
-      reject({ error: 'An error occurred while summarizing the transcript.', details: error });
-    });
+    if (response.data.choices && response.data.choices[0]) {
+      return response.data.choices[0].text.trim();
+    }
 
-    httpsRequest.write(requestBody);
-    httpsRequest.end();
-  });
+    throw new Error('No summary generated.');
+  } catch (error) {
+    throw new Error(`Error summarizing transcript: ${error.message}`);
+  }
 }
 
 module.exports = { summarizeTranscript };
