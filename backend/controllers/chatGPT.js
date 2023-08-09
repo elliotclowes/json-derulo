91889635a51ad92fb65eb51e2a93044244c657d0
elliotclowes <elliot@clowes.me>
2023-08-09 19:27:24 +0100

    +openai node

--- backend/controllers/chatGPT.js
+++ backend/controllers/chatGPT.js
@@ -1,59 +1,27 @@
-const https = require('https');
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config()
 
-const aiApiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-const summarizeTranscript = (transcript) => {
-  return new Promise((resolve, reject) => {
-    const apiKey = aiApiKey;
-    const apiUrl = 'api.openai.com';
-    const path = '/v1/chat/completions';
-
-    const prompt = `I want you to create a summary of the contents of the following transcript: ${transcript}`;
-
-    const requestBody = JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
-      temperature: 0.6,
+const summarizeTranscript = async (transcript) => {
+  try {
+    const prompt = `I want you to create a summary of the contents of the following transcript. If any formatting is need make sure you use HTML. Don't make any mention of the transcript. Just give your summary. This is the transcript: ${transcript}`;
+    
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: prompt}],
     });
 
-    const options = {
-      hostname: apiUrl,
-      path: path,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      },
-    };
-
-    const httpsRequest = https.request(options, (response) => {
-      let data = '';
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-      response.on('end', async () => {
-        const response = JSON.parse(data);
-        const content = response.choices[0].message.content;
-        
-        // Check if the response contains the specific phrase
-        if (content.includes("I'm sorry, but as an AI language model")) {
-          reject({ error: 'Response wasnt properly summarised' });
-        } else {
-          // Resolve the promise with the result
-          resolve(content);
-        }
-      });
-    });
-
-    httpsRequest.on('error', (error) => {
-      // Reject the promise if an error occurs
-      reject({ error: 'An error occurred while getting word count.', details: error });
-    });
-
-    httpsRequest.write(requestBody);
-    httpsRequest.end();
-  });
+    const content = chatCompletion.data.choices[0].message;
+    
+    return content;
+    
+  } catch (error) {
+    throw { error: 'An error occurred while getting word count.', details: error };
+  }
 };
 
 module.exports = { summarizeTranscript };
