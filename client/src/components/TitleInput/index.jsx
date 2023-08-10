53fd0c24a3f8ef5affa87b2acf937b7cfc3360e0
Yaasif Mohammed <YaasifMohammed1@gmail.com>
2023-08-10 12:49:56 +0100

    firebase database works

--- client/src/components/TitleInput/index.jsx
+++ client/src/components/TitleInput/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import './TitleInput.css'; 
+import './TitleInput.css';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { app } from '../../../firebase-config'; 
 
 const predefinedTags = ['Step by step Guide', 'Tutorial', 'Coding', 'Video', 'Food'];
 
 const TitleInput = () => {
   const [title, setTitle] = useState('');
-  const [visibility, setVisibility] = useState('public'); 
+  const [visibility, setVisibility] = useState('private');
   const [selectedTags, setSelectedTags] = useState([]);
+
+  const db = getFirestore(app);
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -24,65 +28,76 @@ const TitleInput = () => {
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log('Title:', title);
-    console.log('Visibility:', visibility);
-    console.log('Selected Tags:', selectedTags);
-
-    window.location.href = '/recorder';
+  
+    const data = {
+      title: title,
+      visibility: visibility,
+      tags: selectedTags,
+    };
+  
+    try {
+      const docRef = await addDoc(collection(db, 'Sand-box'), data);
+      console.log('Document written with ID:', docRef.id);
+      window.location.href = '/recorder'; 
+    } catch (error) {
+      console.error('Error adding document:', error);
+    }
   };
 
   return (
-    <form className="title-form" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={handleTitleChange}
-          placeholder="Enter title"
-        />
-      </div>
-      <div>
-        <label>Visibility:</label>
-        <label>
+    <div>
+      <form className="title-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
           <input
-            type="radio"
-            value="public"
-            checked={visibility === 'public'}
-            onChange={handleVisibilityChange}
+            type="text"
+            id="title"
+            value={title}
+            onChange={handleTitleChange}
+            placeholder="Enter title"
           />
-          Public
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="private"
-            checked={visibility === 'private'}
-            onChange={handleVisibilityChange}
-          />
-          Private
-        </label>
-      </div>
-      <div className="tag-container">
-        <label>Select Tags:</label>
-        <div className="tag-list">
-          {predefinedTags.map((tag) => (
-            <span
-              key={tag}
-              className={`tag ${selectedTags.includes(tag) ? 'selected' : ''}`}
-              onClick={() => handleTagClick(tag)}
-            >
-              {tag}
-            </span>
-          ))}
         </div>
-      </div>
-      <button type="submit">Create Summary</button>
-    </form>
+        <div>
+          <label>Visibility:</label>
+          <label>
+            <input
+              type="radio"
+              value="public"
+              checked={visibility === 'public'}
+              onChange={handleVisibilityChange}
+            />
+            Public
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="private"
+              checked={visibility === 'private'}
+              onChange={handleVisibilityChange}
+            />
+            Private
+          </label>
+        </div>
+        <div className="tag-container">
+          <label>Select Tags:</label>
+          <div className="tag-list">
+            {predefinedTags.map((tag) => (
+              <span
+                key={tag}
+                className={`tag ${selectedTags.includes(tag) ? 'selected' : ''}`}
+                onClick={() => handleTagClick(tag)}
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button type="submit">Create Summary</button>
+      </form>
+    </div>
   );
-};
+}
 
 export default TitleInput;
