90587a81930611c40bfbd28377a5df32d9bfeb06
elliotclowes <elliot@clowes.me>
2023-08-11 23:07:49 +0100

    Footer updated AGAIN

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,47 +2,48 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="footer footer-center p-10 bg-neutral text-primary-content">
-      <div>
-        <svg width="35" height="35" strokeWidth="1.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#4548f2">
-          <path d="M19 8V2m0 0l3 3m-3-3l-3 3M13 2.05c-.329-.033-.662-.05-1-.05C6.477 2 2 6.477 2 12c0 1.821.487 3.53 1.338 5L2.5 21.5l4.5-.838A9.955 9.955 0 0012 22c5.523 0 10-4.477 10-10 0-.338-.017-.671-.05-1" stroke="#e0d2fe" strokeLinecap="round" strokeLinejoin="round"></path>
-        </svg>
-        <p className="font-bold">Audify.me</p>
-        <p>1-19 Artillery Row, London SW1P 1RT</p>
-        <p>© 2023. All right reserved</p>
-      </div>
-      {/* <div>e0d2fe
-        <span className="footer-title">Made with &hearts; by</span> 
-    <div className="grid grid-flow-col gap-2">
-    <div className="avatar">
-  <div className="w-8 rounded-full ring ring-primary ring-offset-base-100">
-    <img src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Felliot.jpg?alt=media&token=a10db1fa-d64f-4f24-83a3-64e6b541915d" />
-  </div>
-</div>
-<div className="avatar">
-  <div className="w-8 rounded-full ring ring-primary ring-offset-base-100">
-    <img src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Fhasancan.jpg?alt=media&token=bf22e00f-457d-494b-9311-340686592569" />
-  </div>
-</div>
-<div className="avatar">
-  <div className="w-8 rounded-full ring ring-primary ring-offset-base-100">
-    <img src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Famal.jpg?alt=media&token=4d45d0b7-6bd2-4ead-bc5d-493de47ac75c" />
-  </div>
-</div>
-<div className="avatar">
-  <div className="w-8 rounded-full ring ring-primary ring-offset-base-100">
-    <img src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Fdanise.jpg?alt=media&token=ce998916-207d-4a77-b228-982c4c69eb70" />
-  </div>
-</div>
-<div className="avatar">
-  <div className="w-8 rounded-full ring ring-primary ring-offset-base-100">
-    <img src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Fyaasif.jpg?alt=media&token=dc546724-18d3-44f5-bbb7-138c04502bfa" />
-  </div>
-</div>
-    </div>
+    <div className="max-w-1200 mx-auto">
 
-      </div> */}
-    </footer>
+
+<footer className="footer p-10 bg-base-200 text-base-content">
+  <div>
+    <span className="footer-title">Made by</span> 
+    <a className="link link-hover">Elliot</a> 
+    <a className="link link-hover">Hasancan</a> 
+    <a className="link link-hover">Amal</a> 
+    <a className="link link-hover">Danise</a>
+    <a className="link link-hover">Yaasif</a>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </div>
+</footer> 
+<footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+  <div className="items-center grid-flow-col">
+  <svg width="36" height="36" strokeWidth="1.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#4548f2">
+          <path d="M19 8V2m0 0l3 3m-3-3l-3 3M13 2.05c-.329-.033-.662-.05-1-.05C6.477 2 2 6.477 2 12c0 1.821.487 3.53 1.338 5L2.5 21.5l4.5-.838A9.955 9.955 0 0012 22c5.523 0 10-4.477 10-10 0-.338-.017-.671-.05-1" stroke="#202a37" strokeLinecap="round" strokeLinejoin="round"></path>
+        </svg>
+    <p><b>Audify.me</b><br/>Copyright © 2023. All right reserved</p>
+  </div> 
+  <div className="md:place-self-center md:justify-self-end">
+    <div className="grid grid-flow-col gap-4">
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    </div>
+  </div>
+</footer>
+    </div>
   );
 }
 
