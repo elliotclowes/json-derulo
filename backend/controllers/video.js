6272a26b8d3847e982068334627c266cd7704ae3
Danise Rosales <daniserosales7@gmail.com>
2023-08-14 12:25:38 +0100

    user_id, timestamp and summary are now being sent to firebase

--- backend/controllers/video.js
+++ backend/controllers/video.js
@@ -5,25 +5,26 @@ const db = admin.firestore();
 
 async function fetchSubtitles(req, res) {
   const youtubeUrl = req.body.url;
-  const user_id = req.body.user_id; // Extract user_id from the request body
-  const timestamp = req.body.timestamp; 
+  const user_id = req.body.user_id
+  console.log(user_id,'fox')
+  console.log(req.body, 'chicharon')
+ // Extract user_id from the request body
+  const timestamp = req.body.timestamp; // Extract timestamp from the request body
   const apiUrl = "http://178.128.39.115:8000/download_subtitles";
   
   try {
     const response = await axios.post(apiUrl, { url: youtubeUrl });
     const transcriptData = response.data; // Assuming this is the data received from the API
-
    
     const transcript = JSON.stringify(transcriptData);
     
     console.log(transcript, 'fish');
 
-    
     const summary = await summarizeTranscript(transcript); 
     const summaryRef = db.collection('summaries.fish').doc();
     await summaryRef.set({ 
       summary: summary,
-      user_id: user_id,
+      user_id: user_id, // Include the fetched user ID
       timestamp: timestamp
     });
     console.log(summary, 'banana');
--- client/src/components/YoutubeUpload/index.jsx
+++ client/src/components/YoutubeUpload/index.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+
 import './index.css';
+
 
 function App() {
   const [youtubeUrl, setYoutubeUrl] = useState('');
   const [subtitles, setSubtitles] = useState('');
   const [isLoading, setIsLoading] = useState(false);  
-  const [timestamp, setTimestamp] = useState('');
+  const currentTimestamp = new Date();
+  const formattedTimestamp = new Date(currentTimestamp).toLocaleString('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZoneName: 'short'
+});
 
   const handleProcessVideo = async () => {
     try {
@@ -13,18 +24,29 @@ function App() {
       const getuserID = async () => {
         const token =localStorage.getItem('token');
         if(!token) return null;
-        const response = await fetch (`http://localhost:3000/token/get${token}`);
+        const response = await fetch (`http://localhost:3000/token/get/${token}`);
         const data = await response.json();
-        return data.user_id.toString();
+        console.log(data, 'fish')
+        return data.user_id.toString()
       };
+      console.log( await getuserID(),'hello')
+      const user_id = await getuserID(); // Fetch the user ID
+      if (!user_id) {
+        console.error("Failed to fetch user ID.");
+        setIsLoading(false); // Set loading to false in case of an error
+        return;
+      }
       const response = await fetch('http://localhost:3000/video/fetch_subtitles', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url: youtubeUrl,
-        user_Id: getuserID })
-      });
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        url: youtubeUrl,
+        user_id: user_id,
+        timestamp: formattedTimestamp
+      })
+});
 
       const data = await response.json();
       console.log(data);
