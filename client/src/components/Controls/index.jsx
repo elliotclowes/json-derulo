1e09c5eeef2a6ffc531f0b54520d58492bc24a57
elliotclowes <elliot@clowes.me>
2023-08-14 15:54:25 +0100

    Removed play, pause and save button

--- client/src/components/Controls/index.jsx
+++ client/src/components/Controls/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const Controls = ({ handlers, isRecording, isPaused }) => (
+const Controls = ({ handlers, isRecording /* , isPaused Remove this part */ }) => (
   <section className="experiment py-5">
     <label htmlFor="time-interval">Time Interval (seconds):</label>
     <input type="text" id="time-interval" defaultValue="60" className="border p-2 my-2 rounded" />
@@ -14,9 +12,6 @@ const Controls = ({ handlers, isRecording, isPaused }) => (
 
     <button id="start-recording" onClick={handlers.startRecording} disabled={isRecording} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50">Start</button>
     <button id="stop-recording" onClick={handlers.stopRecording} disabled={!isRecording} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50">Stop</button>
-    <button id="pause-recording" onClick={handlers.pauseRecording} disabled={!isRecording || isPaused} className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50">Pause</button>
-    <button id="resume-recording" onClick={handlers.resumeRecording} disabled={!isRecording || !isPaused} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50">Resume</button>
-    <button id="save-recording" onClick={handlers.saveRecording} disabled={!isRecording} className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50">Save</button>
   </section>
 );
 
