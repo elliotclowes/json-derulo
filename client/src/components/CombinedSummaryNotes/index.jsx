e7eb15b83c9d5f69e7171e9f3c540fe3335dfc0f
elliotclowes <elliot@clowes.me>
2023-08-14 19:14:53 +0100

    Summary changes so they're side by side

--- client/src/components/CombinedSummaryNotes/index.jsx
+++ client/src/components/CombinedSummaryNotes/index.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getFirestore, collection, doc, updateDoc, onSnapshot } from 'firebase/firestore';
 import { app } from '/firebase-config.js';
-import TextEditor from '../../components/TextEditor';
+import { Footer, AudioRecorder, TextEditor, WriteComment, InfoBox } from "../../components";
+import { BellIcon } from '@heroicons/react/24/outline'
 
 function CombinedSummaryNotes() {
   const { documentId } = useParams();
@@ -50,20 +51,69 @@ function CombinedSummaryNotes() {
   };
 
   return (
-    <div className="container mx-auto px-4">
-      <div id="blocks-display" className="space-y-4">
-        {blocks.map((blockText, index) => (
-          <div key={index} className="border p-4 rounded">
+    <>
+
+<div className="flex min-h-full flex-col">
+        <header className="shrink-0 bg-gray-900">
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+            <div className="flex items-center gap-x-8">
+              <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your profile</span>
+                <img
+                  className="h-8 w-8 rounded-full bg-gray-800"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </header>
+
+
+
+{/* Wrapper */}
+<div className="mx-auto w-full max-w-7xl grow xl:px-2">
+      {blocks.map((blockText, index) => (
+        <div key={index} className="lg:flex">
+          {/* Left sidebar */}
+          <div className="border-b border-gray-200 px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-6">
+            <p>This will be the left.</p>
+          </div>
+
+          {/* Main content */}
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
             <TextEditor 
               document={blockText} 
               onChange={(newText) => updateSummaryBlock(`block${index + 1}`, newText)} 
               onSubmit={(newText) => handleBlockSubmit(index, newText)} 
             />
           </div>
-        ))}
-      </div>
+
+          {/* Right sidebar */}
+          <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6">
+            <p>This will be the right.</p>
+          </div>
+        </div>
+      ))}
     </div>
+    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+      <AudioRecorder documentId={documentId} />
+    </div>
+    </div>
+    </>
   );
 }
-
 export default CombinedSummaryNotes;
+
+
+
+
