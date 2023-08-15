2b8d8e699577f45ef28c3040866b96daaffac54f
Elliot Clowes <elliot@clowes.me>
2023-08-15 11:40:03 +0100

    Update index.jsx

--- client/src/components/SignupForm/index.jsx
+++ client/src/components/SignupForm/index.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignupForm() {
   const [form, setForm] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    username: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
     teacher: false,
-    password: '',
+    password: "",
   });
 
   const [emailError, setEmailError] = useState('');
@@ -34,21 +33,17 @@ export default function SignupForm() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log(JSON.stringify(form));
-    const res = await fetch('http://localhost:3000/user/register', {
-      method: 'POST',
+    const res = await fetch("http://localhost:3000/user/register", {
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify(form),
     });
 
     if (res.ok) {
       const user = await res.json();
-      console.log(user);
-      alert(
-        'Register Successfully! Verification Email has been sent to your email. Please verify your account before enjoying our app.'
-      );
+      alert("Register Successfully! Verification Email has been sent to your email. Please verify your account before enjoying our app.");
     } else {
       const errorResponse = await res.json();
       if (errorResponse.error === 'Username already registered.') {
@@ -62,96 +57,112 @@ export default function SignupForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="flex space-x-4">
-        <div className="w-1/2">
-          <label htmlFor="firstName" className="block text-sm font-medium">
-            First name
-          </label>
-          <input
-            type="text"
-            placeholder="firstname"
-            id="firstName"
-            name="firstName"
-            onChange={handleChange}
-            className="input input-bordered w-full"
+    <div className="flex min-h-full flex-1">
+            <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Fsignup-narrow-min.jpg?alt=media&token=0acf4790-cb6a-494d-b40d-d442520bfce8"
+            alt=""
           />
         </div>
-        <div className="w-1/2">
-          <label htmlFor="lastName" className="block text-sm font-medium">
-            Last name
-          </label>
-          <input
-            type="text"
-            placeholder="lastName"
-            id="lastName"
-            name="lastName"
-            onChange={handleChange}
-            className="input input-bordered w-full"
-          />
+      <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          
+        <div>
+              <img
+                className="h-10 w-auto"
+                src="https://firebasestorage.googleapis.com/v0/b/learnt-me-test.appspot.com/o/manual%2Flogo.svg?alt=media&token=1b976e10-5cf3-42e0-827a-136ced55ba58"
+                alt="Audify.me"
+              />
+              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign up for an account
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">
+                Already have an account?{' '}
+                <a href="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  Login here
+                </a>
+              </p>
+            </div>
+          <div className="mt-10">
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="flex space-x-4">
+                <div className="w-1/2">
+                  <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    onChange={handleChange}
+                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+                <div className="w-1/2">
+                  <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    onChange={handleChange}
+                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="teacher"
+                    name="teacher"
+                    type="checkbox"
+                    onChange={handleChange}
+                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label htmlFor="teacher" className="ml-3 block text-sm leading-6 text-gray-700">Are you a teacher?</label>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <div>
-        <label htmlFor="email" className="block text-sm font-medium">
-          Email
-        </label>
-        <input
-          type="email"
-          placeholder="email@email.com"
-          id="email"
-          name="email"
-          onChange={handleChange}
-          className="input input-bordered w-full"
-        />
-        {emailError && <p className="error-message">{emailError}</p>}
-      </div>
-      <div>
-        <label htmlFor="username" className="block text-sm font-medium">
-          Username
-        </label>
-        <input
-          type="text"
-          placeholder="username"
-          id="username"
-          name="username"
-          onChange={handleChange}
-          className="input input-bordered w-full"
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="block text-sm font-medium">
-          Password
-        </label>
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          name="password"
-          onChange={handleChange}
-          className="input input-bordered w-full"
-        />
-      </div>
-      <div className="input-field">
-        <input type="submit" className="btn" value="Register" />
-      </div>
-      <br />
-      <div>
-        <input
-          type="checkbox"
-          id="teacher"
-          name="teacher"
-          onChange={handleChange}
-        />
-        <label htmlFor="teacher">Are you a teacher?</label>
-      </div>
-      <div className="signup text-center">
-        <span>
-          Already have an account?{' '}
-          <Link to="/login" className="text-blue-500">
-            Login here
-          </Link>
-        </span>
-      </div>
-    </form>
+    </div>
   );
 }
