d148c950dbbfbe49d3ce6d0a7b00c2ab3799bc16
elliotclowes <elliot@clowes.me>
2023-08-15 17:11:21 +0100

    Updated page heading

--- client/src/components/PageHeading/index.jsx
+++ client/src/components/PageHeading/index.jsx
@@ -1,15 +1,15 @@
 export default function PageHeading() {
-    return (
-        <>
-<div className="md:flex md:items-center md:justify-between">
-      <div className="min-w-0 flex-1">
-        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        Your Summaries
-        </h2>
+  return (
+      <div className="px-7">
+          <div className="md:flex md:items-center md:justify-between">
+              <div className="min-w-0 flex-1">
+                  <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                      Your Summaries
+                  </h2>
+              </div>
+          </div>
+          
+          <br /><hr /><br />
       </div>
-    </div>
-      <br></br><br></br>
-      </>
-    )
-  }
-  
+  )
+}
