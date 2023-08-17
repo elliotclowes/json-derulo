5a0702218cae59c7c871038d84e1b5bc87b0633b
elliotclowes <elliot@clowes.me>
2023-08-17 17:58:26 +0100

    Expansion popup

--- client/src/components/MoreDetailButton/index.jsx
+++ client/src/components/MoreDetailButton/index.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { PlusIcon } from '@heroicons/react/20/solid'
+import React, { useEffect, useState, Fragment } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 function AddMoreDetailButton({onDetailButtonClick, document, index}) {
@@ -11,6 +12,7 @@ function AddMoreDetailButton({onDetailButtonClick, document, index}) {
         "content": dataAsString
     })
     const [buttonTag, setButtonTag] = useState("More Detail")
+    const [open, setOpen] = useState(true)
 
     const makeDataJson = async (document) => {
         let compiledData = await dataAsString
@@ -113,16 +115,80 @@ return (
                     className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
                     onClick={handleShortenSummary}
                 >
-                    Expand
+                    &nbsp;Expand&nbsp;
                 </button>
             : 
-                <>
-                    <button onClick={sendBackData}>Yes</button> 
-                    <button onClick={noButton}>No</button>
-                    <p style={{fontSize: '10px',color: 'red'}}>
-                        Warning! Clicking 'Yes' will overwrite your current changes and produce a new summary
-                    </p>
-                </>
+            <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+        
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                                    <button
+                                        type="button"
+                                        className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        {/* Assuming you have the XMarkIcon imported */}
+                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+                                </div>
+        
+                                <div className="mt-3 text-center sm:text-left">
+                                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                        Confirm Expansion
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
+                                            <b>Warning:</b>  expanding your summary produces new text, so any formatting or styling will be removed.
+                                        </p>
+                                    </div>
+                                </div>
+        
+                                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                        onClick={sendBackData}
+                                    >
+                                        Expand
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                        onClick={noButton}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
         }
     </>
 );
