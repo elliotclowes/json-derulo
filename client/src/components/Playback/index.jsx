76b7ede86041db9b0bfa10e6d95683d6693148a4
elliotclowes <elliot@clowes.me>
2023-08-17 10:54:33 +0100

    Hide audioplayer

--- client/src/components/Playback/index.jsx
+++ client/src/components/Playback/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getFirestore, collection, doc, onSnapshot } from 'firebase/firestore';
 import { app } from '/firebase-config.js';
+import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/20/solid';
 
 export default function Playback({ documentId, blockId }) {
-  const [audioUrl, setAudioUrl] = useState('');  // Use useState to keep track of the audio URL
-  
+  const [audioUrl, setAudioUrl] = useState('');  
+  const [isMuted, setIsMuted] = useState(true);  // Initialize as true since audio isn't playing initially
+  const audioRef = useRef(null);  // Create a ref for the audio element
 
   const db = getFirestore(app);
 
@@ -12,32 +14,45 @@ export default function Playback({ documentId, blockId }) {
     const summariesCollection = collection(db, 'summaries');
     const docRef = doc(summariesCollection, documentId);
 
-    // Set up the real-time listener
     const unsubscribe = onSnapshot(docRef, (docSnapshot) => {
         if (docSnapshot.exists) {
             const data = docSnapshot.data();
             if (data.blocks && data.blocks[blockId] && data.blocks[blockId].audioURL) {
-                setAudioUrl(data.blocks[blockId].audioURL);  // Update state with the new audio URL
+                setAudioUrl(data.blocks[blockId].audioURL);
             }
         }
     });
 
-    // Cleanup function
     return () => {
         unsubscribe();
     };
 
   }, [documentId, blockId]);
 
-  
-  console.log("🚀 ~ file: index.jsx:7 ~ Playback ~ audioUrl:", audioUrl)
+  const handleButtonClick = () => {
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsMuted(false);
+    } else {
+      audioRef.current.pause();
+      setIsMuted(true);
+    }
+  }
+
   return (
     <>
-      <audio className="w-64" controls autoPlay key={audioUrl}>
-    <source src={audioUrl} type="audio/wav" />
-    Your browser does not support the audio element.
-</audio>
+      <button
+        type="button"
+        className="rounded-full bg-indigo-600 p-1.5 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        onClick={handleButtonClick}
+      >
+        {isMuted ? <SpeakerWaveIcon className="h-5 w-5" aria-hidden="true" /> : <SpeakerXMarkIcon className="h-5 w-5" aria-hidden="true" />}
+      </button>
 
+      <audio ref={audioRef} className="hidden" key={audioUrl}>  {/* Remove the autoPlay attribute */}
+        <source src={audioUrl} type="audio/wav" />
+        Your browser does not support the audio element.
+      </audio>
     </>
   )
 }
