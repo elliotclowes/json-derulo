e13bdc2d2c531c77a686ecb3e0778caf073f52b2
elliotclowes <elliot@clowes.me>
2023-08-17 18:25:02 +0100

    Learn this next improved

--- client/src/components/AudioRecorder/index.jsx
+++ client/src/components/AudioRecorder/index.jsx
@@ -3,11 +3,18 @@ import Controls from '../Controls';
 import AudioContainer from '../AudioContainer';
 
 const AudioRecorder = ({ documentId }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [nextSteps, setNextSteps] = useState([]);
   const [mediaRecorder, setMediaRecorder] = useState(null);
 
   const [audiosContainer, setAudiosContainer] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
+
+  const [stopRecordingCount, setStopRecordingCount] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+
 
   const captureUserMedia = (mediaConstraints, successCallback, errorCallback) => {
     navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
@@ -92,6 +99,7 @@ const AudioRecorder = ({ documentId }) => {
       mediaRecorder.stream.stop();
       setIsRecording(false);
       setIsPaused(false);
+      setStopRecordingCount(prevCount => prevCount + 1);
     },
     pauseRecording: () => {
       mediaRecorder.pause();
@@ -107,12 +115,99 @@ const AudioRecorder = ({ documentId }) => {
     },
   };
 
+
+
+  const handleLearnMore = async () => {
+    try {
+      setIsLoading(true);
+      const prompt = "Please provide 3 bullet points on what to learn next and make them 1-4 word each :";
+      const combinedText = blocks.join(' ');
+      const response = await fetch('http://localhost:3000/audio/chatgpt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt: prompt,
+          content: combinedText
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Error fetching next steps');
+      }
+
+      const contentType = response.headers.get('content-type');
+
+      if (contentType && contentType.includes('application/json')) {
+        const data = await response.json();
+
+        if (data && data.nextSteps) {
+          setNextSteps(data.nextSteps);
+        } else {
+          throw new Error('Invalid JSON response');
+        }
+      } else {
+        const plainTextResponse = await response.text();
+        const sentences = plainTextResponse.split('. '); 
+        setNextSteps(sentences);
+      }
+
+    } catch (error) {
+      console.error('Error fetching next steps:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="container mx-auto px-4">
-      <Controls handlers={handlers} isRecording={isRecording} isPaused={isPaused} />
-      <AudioContainer />
-    </div>
+    <>
+      <div className="container mx-auto px-4">
+        <Controls handlers={handlers} isRecording={isRecording} isPaused={isPaused} />
+        <AudioContainer />
+        <br></br>
+      </div>
+  
+      {/* Conditionally render the "Learn this next..." section only when isRecording is false */}
+      {stopRecordingCount > 0 && !isRecording && (
+        <div className="bg-gray-100 py-24 sm:py-6 rounded-lg shadow">
+          <div className="mx-auto max-w-7xl shadowpx-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:text-center">
+              <p className="mt-2 text-2xl font-bold tracking-tight text-blue-700 sm:text-3xl">
+                Learn this next...
+              </p>
+              <p className="mt-6 text-m leading-8 text-gray-700">
+                We'll use AI to analyse your summary and give<br></br> you some suggestions on what you should learn next!
+              </p>
+  
+              <button 
+                type="button" 
+                className="rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-4" 
+                onClick={handleLearnMore} 
+                disabled={isLoading}
+              >
+                Look into the future
+              </button>
+  
+              <div className="nextSteps mt-4">
+                {nextSteps.length > 0 && (
+                  <div>
+                    <h2>What to Learn Next:</h2>
+                    <ul>
+                      {nextSteps.map((step, index) => (
+                        <li key={index}>{step}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
+  
 };
 
 export default AudioRecorder;
--- client/src/components/CombinedSummaryNotes/index.jsx
+++ client/src/components/CombinedSummaryNotes/index.jsx
@@ -15,9 +15,7 @@ function CombinedSummaryNotes() {
   const [dataFromDetailButton, setDataFromDetailButton] = useState("")
   const [shortSummary, setShortSummary] = useState(false)
   const db = getFirestore(app);
-  const [isLoading, setIsLoading] = useState(false);
-  const [nextSteps, setNextSteps] = useState([]);
-  const [detailIndex, setDetailIndex] = useState(1)
+  const [detailIndex, setDetailIndex] = useState(1);
 
 
   const updateSummaryBlock = async (blockId, newText) => {
@@ -60,48 +58,6 @@ function CombinedSummaryNotes() {
     updateSummaryBlock(`block${blockId + 1}`, newText);
   };
 
-  const handleLearnMore = async () => {
-    try {
-      setIsLoading(true);
-      const prompt = "Please provide 3 bullet points on what to learn next and make them 1-4 word each :";
-      const combinedText = blocks.join(' ');
-      const response = await fetch('http://localhost:3000/audio/chatgpt', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          prompt: prompt,
-          content: combinedText
-        })
-      });
-
-      if (!response.ok) {
-        throw new Error('Error fetching next steps');
-      }
-
-      const contentType = response.headers.get('content-type');
-
-      if (contentType && contentType.includes('application/json')) {
-        const data = await response.json();
-
-        if (data && data.nextSteps) {
-          setNextSteps(data.nextSteps);
-        } else {
-          throw new Error('Invalid JSON response');
-        }
-      } else {
-        const plainTextResponse = await response.text();
-        const sentences = plainTextResponse.split('. '); 
-        setNextSteps(sentences);
-      }
-
-    } catch (error) {
-      console.error('Error fetching next steps:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   const handleDetailButtonClick = async (data, index) => {
     console.log("preUpdate",dataFromDetailButton)
@@ -198,38 +154,6 @@ function CombinedSummaryNotes() {
 
 
         <AudioRecorder documentId={documentId} />
-          
-        <div className="bg-gray-100 py-24 sm:py-6 rounded-lg shadow">
-  <div className="mx-auto max-w-7xl shadowpx-6 lg:px-8">
-    <div className="mx-auto max-w-2xl lg:text-center">
-      <p className="mt-2 text-2xl font-bold tracking-tight text-blue-700 sm:text-3xl">
-        Learn this next...
-      </p>
-      <p className="mt-6 text-m leading-8 text-gray-700">
-        We'll use AI to analyse your summary and give<br></br> you some suggestions on what you should learn next!
-      </p>
-      
-      <button type="button" className="rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-4" onClick={handleLearnMore} disabled={isLoading}>
-        Look into the future
-      </button>
-
-  
-      <div className="nextSteps mt-4">
-        {nextSteps.length > 0 && (
-          <div>
-            <h2>What to Learn Next:</h2>
-            <ul>
-              {nextSteps.map((step, index) => (
-                <li key={index}>{step}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
-  </div>
-</div>
-
 
 </div>
     </div>
